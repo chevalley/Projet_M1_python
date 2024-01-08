3d603e7b97f089fe4f85771eac11a67f63f2e8a3
yacine mahiouz <yacin912@gmail.com>
2024-01-08 09:25:26 +0100

    UML ajouté

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def reconf_etagere():
     return redirect("/modify_cave")
 
 if __name__ == "__main__" : 
-    app.run(debug=True, host = "0.0.0.0", port = 80)
+    app.run(debug=True, host = "0.0.0.0", port = "8080")
